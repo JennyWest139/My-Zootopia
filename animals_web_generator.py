0835b8611a1ef40629c3fce4d660cf1343b3447b
Jennifer Westphal <jennifer.westphal.world@gmail.com>
2026-04-06 16:20:57 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,13 +13,19 @@ def load_template(file_path):
         return html_file.read()
 
 
+def write_html_file(file_path, content):
+    """Creates and fills an HTML file with text"""
+    with open(file_path, "w", encoding="utf-8") as result_html_file:
+        result_html_file.write(content)
+        return None
+
+
 def main():
     """Main function"""
 
     animals_data = load_data("animals_data.json")
-
     html_template = load_template("animals_template.html")
-    print(html_template)  # nur zum Testen
+    result_html_file_path = "animals.html"
 
     output = ""  # define an empty string
     for animal in animals_data:
@@ -36,7 +42,7 @@ def main():
             output += f"Location: {locations[0]}\n"
 
     html_result = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
-    print(html_result)
+    write_html_file(result_html_file_path, html_result)
 
 
 if __name__ == "__main__":
