685aef468c5efa55d2752fbbd9546f2ccb106e9e
Jennifer Westphal <jennifer.westphal.world@gmail.com>
2026-04-06 16:06:09 +0200

    Read html and add animal list

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,8 +3,14 @@ import json
 
 def load_data(file_path):
     """Loads a JSON file"""
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+    with open(file_path, "r") as json_file:
+        return json.load(json_file)
+
+
+def load_template(file_path):
+    """Loads an HTML template file as text"""
+    with open(file_path, "r", encoding="utf-8") as html_file:
+        return html_file.read()
 
 
 def main():
@@ -12,18 +18,25 @@ def main():
 
     animals_data = load_data("animals_data.json")
 
+    html_template = load_template("animals_template.html")
+    print(html_template)  # nur zum Testen
+
+    output = ""  # define an empty string
     for animal in animals_data:
         name = animal.get("name", "Unknown")  # name is compulsory
         characteristics = animal.get("characteristics", {})
         locations = animal.get("locations", [])
 
-        print(f"\nName: {name}")
+        output += f"\nName: {name}\n"
         if "diet" in characteristics:
-            print(f"Diet: {characteristics['diet']}")
+            output += f"Diet: {characteristics['diet']}\n"
         if "type" in characteristics:
-            print(f"Type: {characteristics['type']}")
+            output += f"Type: {characteristics['type']}\n"
         if locations:
-            print(f"Location: {locations[0]}")
+            output += f"Location: {locations[0]}\n"
+
+    html_result = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
+    print(html_result)
 
 
 if __name__ == "__main__":
